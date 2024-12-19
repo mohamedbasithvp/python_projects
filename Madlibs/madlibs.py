bde6b838c7f32a8ed48940af41bdfe6af6430c7d
Basith Tirur <mbvp555@gmail.com>
2024-12-19 14:08:10 +0530

    Madlibs Compeleted

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,6 @@
+adj = input("Adjective: ")
+verb1 = input("varb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+madlib=f"computer programming so {adj}! it makes me so exited all the time because i have to {verb1} stay hydrated and {verb2} like you are {famous_person}"
+print(madlib)
