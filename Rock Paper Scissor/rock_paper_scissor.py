d7c0fd08cd84695a50bd86e2a04d8ac9380d124e
Basith Tirur <mbvp555@gmail.com>
2024-12-19 20:34:55 +0530

    Rock Paper Scissor game completed

---
+++ Rock Paper Scissor/rock_paper_scissor.py
@@ -0,0 +1,21 @@
+import random
+
+def play():
+    user=input("'r' for rock, 'p' for paper, 's' fro scissor" )
+    computer=random.choice(['r', 'p', 's'])
+    print(computer)
+    if is_win(user, computer):
+        return 'You Win!'
+    if is_die(user, computer):
+        return 'Die'
+    return' You Lost!'
+
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+
+def is_die(player,opponent):
+    if (player == 'r' and opponent == 'r') or (player == 's' and opponent == 's') or (player == 'p' and opponent == 'p'):
+        return True
+
+print(play())
